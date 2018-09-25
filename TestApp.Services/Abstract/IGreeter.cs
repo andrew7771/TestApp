9864a2c6392118c83ce9dd83723c91eed70f85f1
Andrii Turianskyi <aturyansky33@gmail.com>
2018-09-25 14:44:16 +0300

    added Services for the app

---
+++ TestApp.Services/Abstract/IGreeter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Services.Abstract
+{
+    interface IGreeter
+    {
+        string SaySomething(string message);
+    }
+}
---
+++ TestApp.Services/Specific/Greeter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Services.Abstract;
+
+namespace TestApp.Services.Specific
+{
+    class Greeter : IGreeter
+    {
+        public string SaySomething(string message)
+        {
+            return message;
+        }
+    }
+}
